9e5b4ce4c4dd53fe71e206b54d9d5804547e844d
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-12-01 12:08:28 -0500

    rendering currency is done

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./Currency.css";
 
 
-const Currency = ()=> {
+const Currency = (props)=> {
+  // console.log(props)
   return (
     <div className="currency">
-      <span className="name-cur">Bitcoin</span>
-      <span className="total-cur">total supply</span>
-      <span className="market-cap-cur">market cap</span>
-      <span className="current-price-cur">current price</span>
+      <span className="name-cur">{props.children.name}</span>
+      <span className="total-cur">{props.children.totalSupply}</span>
+      <span className="market-cap-cur">{props.children.marketCap}</span>
+      <span className="current-price-cur">${props.children.currentPrice}</span>
       <div className="row-cur"></div>
-    
     </div>
   );
 } 
--- src/conteiners/Table/Table.js
+++ src/conteiners/Table/Table.js
@@ -1,9 +1,31 @@
-import React from "react";
-import Currency from "../../components/Currency/Currency"
-import "./Table.css"
+import React, { useState, useEffect } from "react";
+import Currency from "../../components/Currency/Currency";
+import "./Table.css";
 
-const Table = () => {
-  
+const Table = (props) => {
+  // const {onCurrencyList} = props
+  const [currencyList, setCurrencyList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad")
+      .then((response) => response.json())
+      .then((data) => {
+        const currencies = data.slice(0, 5);
+        console.log(currencies);
+        const currencyArray = [];
+        for (const currency of currencies) {
+          currencyArray.push({
+            id: currency.id,
+            name: currency.name,
+            totalSupply: currency.total_supply,
+            marketCap: currency.market_cap,
+            currentPrice: currency.current_price,
+          });
+        }
+        setCurrencyList(currencyArray);
+      });
+  }, [setCurrencyList]);
+  console.log(currencyList[1]);
   return (
     <div className="table">
       <div className="title-row">
@@ -12,13 +34,9 @@ const Table = () => {
         <span className="market-cap">market cap</span>
         <span className="current-price">current price</span>
       </div>
-
-
-      <Currency />
-      <Currency />
-      <Currency />
-      <Currency />
-      <Currency />
+      {currencyList.map((cur) => (
+        <Currency key={cur.id}>{cur}</Currency>
+      ))}
     </div>
   );
 };
